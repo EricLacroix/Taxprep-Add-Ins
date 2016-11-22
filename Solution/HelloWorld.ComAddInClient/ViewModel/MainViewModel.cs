6a757db0a7ed757628a897e4aae8f784313ce92e
NA\Eric.Lacroix <eric.lacroix@wolterskluwer.com>
2016-11-22 10:25:48 -0500

    Update Nuget

--- Solution/HelloWorld.ComAddInClient/ViewModel/MainViewModel.cs
+++ Solution/HelloWorld.ComAddInClient/ViewModel/MainViewModel.cs
@@ -1,152 +1,34 @@
-using System;
-using System.Configuration;
-using System.Runtime.InteropServices;
-using System.Threading.Tasks;
 using GalaSoft.MvvmLight;
-using GalaSoft.MvvmLight.Command;
-using NLog;
-using TaxprepAddinAPI;
-using WKCA.Com.Utils;
 
 namespace HelloWorld.ComAddInClient.ViewModel
 {
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// You can also use Blend to data bind with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
     public class MainViewModel : ViewModelBase
     {
-        private static Logger _logger;
-        private IAppInstance _appInstance;
-        private string _exceptionMessage;
-
-        private string _status;
-
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
         public MainViewModel()
         {
-            _logger = LogManager.GetCurrentClassLogger();
-            Status = "Not connected";
-            TaxPrepFileName = "T1Txp2015.exe";
-            ConnectComAddIn = new RelayCommand(ConnectComAddInExecute, () => Status == "Not connected");
-            ShowMessage = new RelayCommand(ShowMessageExecute, () => Status.StartsWith("Connected"));
-            DisconnectComAddIn = new RelayCommand(DisconnectComAddInExecute, () => Status.StartsWith("Connected"));
-        }
-
-        public string TaxPrepFileName { get; set; }
-
-        public string Status
-        {
-            get { return _status; }
-            set
-            {
-                _status = value;
-                RaisePropertyChanged();
-            }
-        }
-
-        public string ExceptionMessage
-        {
-            get { return _exceptionMessage; }
-            set
-            {
-                _exceptionMessage = value;
-                RaisePropertyChanged();
-            }
-        }
-
-        public RelayCommand ConnectComAddIn { get; set; }
-        public RelayCommand DisconnectComAddIn { get; set; }
-        public RelayCommand ShowMessage { get; set; }
-
-        public async void ConnectComAddInExecute()
-        {
-            _logger.Log(LogLevel.Trace, "Start connection to COM Add-In");
-            try
-            {
-                Status = "Connecting...";
-                await Task.Factory.StartNew(GetTaxPrepApp);
-                var appTitle = ((_appInstance) as IAppTaxApplicationService).GetTitleName();
-                Status = string.Format($"Connected to {appTitle}");
-                _logger.Log(LogLevel.Trace, "Connected to COM Add-In");
-            }
-            catch (Exception ex)
-            {
-                Status = "Connection failed";
-                ExceptionMessage = ex.Message;
-                _logger.Log(LogLevel.Fatal, ex);
-            }
-            ConnectComAddIn.RaiseCanExecuteChanged();
-            ShowMessage.RaiseCanExecuteChanged();
-            DisconnectComAddIn.RaiseCanExecuteChanged();
-        }
-
-        public void ShowMessageExecute()
-        {
-            _logger.Log(LogLevel.Trace, "ShowMessageExecute called");
-            try
-            {
-                _appInstance.AddLog("Hello world");
-            }
-            catch (Exception ex)
-            {
-                ExceptionMessage = ex.Message;
-                _logger.Log(LogLevel.Fatal, ex);
-            }
-        }
-
-        public void DisconnectComAddInExecute()
-        {
-            Marshal.ReleaseComObject(_appInstance);
-            _appInstance = null;
-
-            Status = "Not connected";
-            ConnectComAddIn.RaiseCanExecuteChanged();
-            ShowMessage.RaiseCanExecuteChanged();
-            DisconnectComAddIn.RaiseCanExecuteChanged();
-        }
-
-        private void GetTaxPrepApp()
-        {
-            if (_appInstance != null)
-            {
-                return;
-            }
-
-            GetTaxPrepAppUsingGuid();
-            //GetTaxPrepAppUsingProgId();
-        }
-
-        /// <summary>
-        ///     Uses GUID to create COM object. If COM object is registered in the registry corresponding process will be started
-        ///     automatically.
-        ///     If COM object is not registered in the registry, but corresponding process is already running will connect to this
-        ///     process.
-        /// </summary>
-        private void GetTaxPrepAppUsingGuid()
-        {
-            var helper = new ComRegistrationHelper();
-            var guidTemplate = ConfigurationManager.AppSettings["TaxPrepComGuidTemplate"];
-            var guid = helper.CreateApplicationSpecificGuidFromTemplate(guidTemplate, TaxPrepFileName);
-            var type = Type.GetTypeFromCLSID(guid, true);
-            dynamic comAccessProvider = Activator.CreateInstance(type);
-
-            dynamic appInstance = comAccessProvider.GetAppInstance();
-            _appInstance = (IAppInstance) appInstance;
-
-            Marshal.ReleaseComObject(comAccessProvider);
-        }
-
-        /// <summary>
-        ///     Uses ProgId to create COM object. If COM object is registered in the registry corresponding process will be started
-        ///     automatically.
-        ///     If COM object is not registered in the registry, fails to connect even if corresponding process is running.
-        /// </summary>
-        private void GetTaxPrepAppUsingProgId()
-        {
-            var helper = new ComRegistrationHelper();
-            var typeName = ConfigurationManager.AppSettings["TaxPrepComTypeName"];
-            var progId = helper.GetProgId(TaxPrepFileName, typeName);
-            var type = Type.GetTypeFromProgID(progId);
-            dynamic comAccessProvider = Activator.CreateInstance(type);
-
-            dynamic appInstance = comAccessProvider.GetAppInstance();
-            _appInstance = (IAppInstance) appInstance;
+            ////if (IsInDesignMode)
+            ////{
+            ////    // Code runs in Blend --> create design time data.
+            ////}
+            ////else
+            ////{
+            ////    // Code runs "for real"
+            ////}
         }
     }
 }
--- Solution/Sample.ComAddInClient/ViewModel/MainViewModel.cs
+++ Solution/Sample.ComAddInClient/ViewModel/MainViewModel.cs
@@ -1,122 +1,34 @@
-using System;
-using System.Configuration;
-using System.Threading.Tasks;
 using GalaSoft.MvvmLight;
-using GalaSoft.MvvmLight.Command;
-using NLog;
-using Sample.ComAddInClient.Services;
-using TaxprepAddinAPI;
-using WKCA.Com.Utils;
 
 namespace Sample.ComAddInClient.ViewModel
 {
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// You can also use Blend to data bind with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
     public class MainViewModel : ViewModelBase
     {
-        private static Logger _logger;
-        private readonly SampleUIService _uiService;
-        private IAppInstance _appInstance;
-        private string _exceptionMessage;
-
-        private string _status;
-
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
         public MainViewModel()
         {
-            _uiService = new SampleUIService();
-            _logger = LogManager.GetCurrentClassLogger();
-            Status = "Not connected";
-            TaxPrepFileName = "T1Txp2015.exe";
-            ConnectComAddIn = new RelayCommand(ConnectComAddInExecute, () => Status == "Not connected");
-        }
-
-        public string TaxPrepFileName { get; set; }
-
-        public string Status
-        {
-            get { return _status; }
-            set
-            {
-                _status = value;
-                RaisePropertyChanged();
-            }
-        }
-
-        public string ExceptionMessage
-        {
-            get { return _exceptionMessage; }
-            set
-            {
-                _exceptionMessage = value;
-                RaisePropertyChanged();
-            }
-        }
-
-        public RelayCommand ConnectComAddIn { get; set; }
-
-        public async void ConnectComAddInExecute()
-        {
-            _logger.Log(LogLevel.Trace, "Start connection to COM Add-In");
-            try
-            {
-                Status = "Connecting...";
-                await Task.Factory.StartNew(GetTaxPrepApp);
-                var appTitle = ((_appInstance) as IAppTaxApplicationService).GetTitleName();
-                Status = string.Format($"Connected to {appTitle}");
-                _logger.Log(LogLevel.Trace, "Connected to COM Add-In");
-                _uiService.Init(_appInstance);
-            }
-            catch (Exception ex)
-            {
-                Status = "Connection failed";
-                ExceptionMessage = ex.Message;
-                _logger.Log(LogLevel.Fatal, ex);
-            }
-            ConnectComAddIn.RaiseCanExecuteChanged();
-        }
-
-        private void GetTaxPrepApp()
-        {
-            if (_appInstance != null)
-            {
-                return;
-            }
-
-            GetTaxPrepAppUsingGuid();
-            //GetTaxPrepAppUsingProgId();
-        }
-
-        /// <summary>
-        ///     Uses GUID to create COM object. If COM object is registered in the registry corresponding process will be started
-        ///     automatically.
-        ///     If COM object is not registered in the registry, but corresponding process is already running will connect to this
-        ///     process.
-        /// </summary>
-        private void GetTaxPrepAppUsingGuid()
-        {
-            var helper = new ComRegistrationHelper();
-            var guidTemplate = ConfigurationManager.AppSettings["TaxPrepComGuidTemplate"];
-            var guid = helper.CreateApplicationSpecificGuidFromTemplate(guidTemplate, TaxPrepFileName);
-            var type = Type.GetTypeFromCLSID(guid, true);
-            dynamic comAccessProvider = Activator.CreateInstance(type);
-
-            dynamic appInstance = comAccessProvider.GetAppInstance();
-            _appInstance = (IAppInstance) appInstance;
-        }
-
-        /// <summary>
-        ///     Uses ProgId to create COM object. If COM object is registered in the registry corresponding process will be started
-        ///     automatically.
-        ///     If COM object is not registered in the registry, fails to connect even if corresponding process is running.
-        /// </summary>
-        private void GetTaxPrepAppUsingProgId()
-        {
-            var helper = new ComRegistrationHelper();
-            var typeName = ConfigurationManager.AppSettings["TaxPrepComTypeName"];
-            var progId = helper.GetProgId(TaxPrepFileName, typeName);
-            var type = Type.GetTypeFromProgID(progId);
-            dynamic comAccessProvider = Activator.CreateInstance(type);
-
-            dynamic appInstance = comAccessProvider.GetAppInstance();
-            _appInstance = (IAppInstance) appInstance;
+            ////if (IsInDesignMode)
+            ////{
+            ////    // Code runs in Blend --> create design time data.
+            ////}
+            ////else
+            ////{
+            ////    // Code runs "for real"
+            ////}
         }
     }
 }
